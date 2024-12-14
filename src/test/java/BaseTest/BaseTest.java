77e765c405a506121083315210fefa5fe0232c9a
Vlad Avgustinovich <vladavgustinovich3800@gmail.com>
2024-12-14 13:39:46 +0300

    Try #1 new code

--- src/test/java/BaseTest/BaseTest.java
+++ src/test/java/BaseTest/BaseTest.java
@@ -6,20 +6,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.time.Duration;
 
-public  class BaseTest {
+public class BaseTest {
     public static WebDriver driver;
 
     @BeforeAll
     static void setup() {
         // Определение типа драйвера
-        String driverType = System.getProperty("type.driver", "remote");
+        String driverType = System.getProperty("type.driver", "local");
         if ("remote".equalsIgnoreCase(driverType)) {
             initRemoteDriver();
+        } else {
+            initLocalDriver();
         }
+
         driver.manage().window().maximize();
         driver.get("https://qualit.applineselenoid.fvds.ru/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -28,15 +32,29 @@ public  class BaseTest {
 
     private static void initRemoteDriver() {
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setBrowserName(System.getProperty("type.browser", "chrome"));
-        capabilities.setVersion("109.0");
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", false);
+
+        // Получение параметров из системы
+        String browserName = System.getProperty("type.browser", "chrome");
+        String browserVersion = System.getProperty("browser.version", "109.0");
+
+        capabilities.setCapability("browserName", browserName);
+        capabilities.setCapability("browserVersion", browserVersion);
+        capabilities.setCapability("selenoid:options", new DesiredCapabilities() {{
+            setCapability("enableVNC", true);
+            setCapability("enableVideo", false);
+        }});
+
         try {
             driver = new RemoteWebDriver(URI.create(System.getProperty("selenoid.url", "http://jenkins.applineselenoid.fvds.ru:4444/wd/hub/")).toURL(), capabilities);
         } catch (MalformedURLException e) {
             throw new RuntimeException("Ошибка подключения к Selenoid", e);
         }
+    }
+
+    private static void initLocalDriver() {
+        // Установка системного свойства для ChromeDriver
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver");
+        driver = new ChromeDriver();
     }
 
     @AfterAll
--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -1,25 +1,20 @@
 package steps;
 
-
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.BeforeAll;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import pages.HomePage;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.time.Duration;
 
 public class Hooks {
 
     public static WebDriver driver;
-    private static Process process;
     private static Connection connection;
     private static final String url = "jdbc:h2:tcp://qualit.applineselenoid.fvds.ru:9092/mem:testdb";
     private static final String user = "user";
     private static final String password = "pass";
-
 
     @BeforeAll(order = 2)
     public static void dataBaseConnection() {
@@ -31,19 +26,6 @@ public class Hooks {
             throw new RuntimeException("Ошибка подключения к базе данных: " + e.getMessage());
         }
     }
-    @BeforeAll(order = 1)
-    public static void setUp() {
-        if (driver == null) {
-            System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver");
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.get("https://qualit.applineselenoid.fvds.ru/");
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-        }
-    }
-
-
 
     @AfterAll
     public static void tearDown() {
